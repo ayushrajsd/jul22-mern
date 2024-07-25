a3f06d20f2c73ccda42343f4ed7328b34d04dc06
Ayush <ayush.raj.sd@gmail.com>
2024-07-25 11:01:51 +0530

    lecture 2 express and middlewares

--- index.js
+++ index.js
@@ -1,145 +1,84 @@
-// // const fs = require("fs"); // file system module ; Common Js module
-// // // import fs from "fs"; // ES6 module
+const express = require("express");
 
-// // // fs.readFile("file.txt", "utf8", (err, data) => {
-// // //   // error first callback
-// // //   if (err) {
-// // //     console.log(err);
-// // //     return;
-// // //   } else {
-// // //     console.log("data from file", data);
-// // //   }
-// // // });
+// create an express application
+const app = express();
+app.use(express.static("public"));
 
-// // // const content = "This is a new file created by Node.js";
-// // // fs.writeFile("example.txt", content, "utf8", (err) => {
-// // //   if (err) {
-// // //     console.log(err);
-// // //     return;
-// // //   }
-// // //   console.log("File created successfully");
-// // // });
+app.use(express.json()); // for parsing application/json - json data is made available in req.body
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded - form data is made available in req.body
+// example of url endoded form data
+// name=John+Doe&age=30&email=johndoe%40gmail.com
 
-// // // const data = fs.readFileSync("example.txt", "utf8");
-// // // console.log("Data from file", data);
-
-// // // fs.rename("example.txt", "new-example.txt", (err) => {
-// // //   if (err) {
-// // //     console.log(err);
-// // //     return;
-// // //   }
-// // //   console.log("File renamed successfully");
-// // // });
-
-// // // fs.unlink("new-example.txt", (err) => {
-// // //   if (err) {
-// // //     console.log(err);
-// // //     return;
-// // //   }
-// // //   console.log("File deleted successfully");
-// // // });
-
-// // // fs.stat("file1.txt", (err, stats) => {
-// // //   if (err) {
-// // //     console.log("error found", err.message);
-// // //     return;
-// // //   }
-// // //   console.log(stats.size);
-// // //   console.log(stats.isDirectory());
-// // // });
-
-// // // const directoryName = "my-directory";
-// // // fs.mkdir(directoryName, (err) => {
-// // //   if (err) {
-// // //     console.log(err);
-// // //     return;
-// // //   }
-// // //   console.log("Directory created successfully");
-// // // });
-
-// // // const directoryPath = "./my-directory";
-// // // if (fs.existsSync(directoryPath)) {
-// // //   console.log("Directory exists");
-// // // } else {
-// // //   console.log("Directory does not exist");
-// // // }
-
-// // // fs.access
-// // /**
-// //  * path module
-// //  * it is essential when dealing with file paths in different operating systems
-// //  * file path format differs in different operating systems
-// //  * windows: C:\Users\user\Documents\file.txt
-// //  * POSIX: /home/user/Documents/file.txt
-// //  */
-
-// // const path = require("path");
-
-// // const fullPath = path.join("folder", "subFolder", "file.txt");
-// // console.log(fullPath);
-
-// // const absolutePath = path.resolve("folder", "subFolder", "file.txt");
-// // console.log(absolutePath);
-
-// // const fileName = path.basename("./dir/file.txt");
-// // console.log(fileName);
-
-// // const extName = path.extname("./dir/file.txt");
-// // console.log(extName);
-
-// // const normalizedpath = path.normalize("/path/to/../file.txt"); // /path/file.txt  normalizes / resoilves all . and .. in the path
-// // console.log(normalizedpath);
-
-// // const complexPath = "/path/./to/../to/../../file.txt";
-// // const normalizedPath = path.normalize(complexPath);
-// // console.log(normalizedPath);
-
-// const fs = require("fs");
-// const sourceFilePath = "./dir/file.txt";
-// const destinationPath = "./destination-file.txt";
-
-// // create a readable stream from the source file
-// const readStream = fs.createReadStream(sourceFilePath);
-
-// // create a writable stream to the destination file
-// const writeStream = fs.createWriteStream(destinationPath);
-
-// // pipe the data from the read stream to the write stream
-// readStream.pipe(writeStream);
-
-// // handle any errors
-// readStream.on("error", (err) => {
-//   console.log("Error reading file", err.message);
-// });
-
-// writeStream.on("error", (err) => {
-//   console.log("Error writing file", err.message);
-// });
-
-// writeStream.on("finish", () => {
-//   console.log("File copied successfully");
-// });
-
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  // handle incoming requests and send response
-  //   res.setHeader("Content-Type", "text/html"); // mime -types ": maintype / subtype"
-  //   //   res.write("Hello World");
-  //   res.write("<html><head><title>Node.js Server</title></head><body>");
-  //   res.write("<h1>Hello World </h1></body></html>");
-  res.setHeader("Content-Type", "application/json");
-  const jsonData = {
-    message: "Hello World",
-    date: new Date(),
-  };
-  const jsonResponse = JSON.stringify(jsonData);
-  res.write(jsonResponse);
-  res.end();
+app.use((req, res) => {
+  res.status(404).send("Page not found");
+});
+const loggerMiddleware = (req, res, next) => {
+  console.log(
+    `Logged  ${req.url}  ${req.method} -- ${new Date().toISOString()}`
+  );
+  next();
+};
+// app.use(loggerMiddleware);
+// define a route - GET request
+app.get("/", (req, res) => {
+  res.send("Hello, Express , responding to get request");
 });
 
-const port = 8002;
-const host = "localhost";
-server.listen(port, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+app.get("/about", (req, res) => {
+  res.send("this is the about page");
+});
+
+app.post("/data", (req, res) => {
+  console.log(req.body);
+  res.send("Received a post request");
+});
+
+const users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Doe" },
+];
+
+// post endpoint to add a new user
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  const userId = users.length + 1;
+  newUser.id = userId;
+
+  users.push(newUser);
+  res.status(201).json({ message: "User Created", user: newUser });
+});
+
+app.delete("/users/:id", (req, res) => {
+  //   console.log("Request", req);
+  const userId = req.params.id;
+  console.log("User ID", userId);
+
+  // find the user by id
+  const userIndex = users.findIndex((user) => user.id == userId);
+
+  if (userIndex == -1) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  // remove the user
+  users.splice(userIndex, 1);
+  res.json({ message: "User deleted" });
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/special", loggerMiddleware, (req, res) => {
+  res.send("Special page");
+});
+
+app.get("/search", (req, res) => {
+  console.log("Query params", req.query);
+  res.send("Search page");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
